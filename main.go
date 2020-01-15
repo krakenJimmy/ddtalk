16ea1d21b35048bdec994102c9c39a035ff5c3b3
xubo <bo.xu@ifchange.com>
2020-01-15 14:03:33 +0800

    定时任务

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"time"
+
+	"github.com/robfig/cron"
+)
+
 func main() {
-	SendMsg("外卖好了")
+	c := cron.New()
+	c.AddFunc("5 * * * * *", func() {
+		SendMsg("今天吃外卖了吗")
+	})
+
+	c.Start()
+	t1 := time.NewTimer(time.Second * 10)
+	for {
+		select {
+		case <-t1.C:
+			t1.Reset(time.Second * 10)
+		}
+	}
 }
