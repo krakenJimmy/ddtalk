c267e67f27be9ccd7ef08d682d1f9af2857f7221
xubo <bo.xu@ifchange.com>
2020-01-15 11:44:58 +0800

    钉钉推送

---
+++ talk.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type TextMsg struct {
+	MsgType string `json:"msgtype"`
+	Text    struct {
+		Content string `json:"content"`
+	} `json:"text"`
+}
+
+func main() {
+	requestData := TextMsg{
+		MsgType: "text",
+	}
+
+	requestData.Text.Content = "你该点外卖了"
+	requestBody, err := json.Marshal(requestData)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := http.Post("https://oapi.dingtalk.com/robot/send?access_token=6f1369f41eb0b124263ceb458d9cceedd4a85c36c9962ade594af1db92f55fd9", "application/json", bytes.NewBuffer(requestBody))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+	json.Unmarshal(body, &result)
+
+	fmt.Println(result)
+}
